9b09f588f7a9790c8b26449ac382e48c7d839c44
Cedric Karungu <ckarungu921@gmail.com>
2024-03-26 19:23:11 +0200

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coffea App
+
+A brief description of what this project does and who it's for
+
+## Screenshots
+
+<img width="1051" alt="Screenshot " src="./assets/app.png">
--- screens/homeScreen.tsx
+++ screens/homeScreen.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
 	return (
-		<SafeAreaView>
-			<Text>homeScreen</Text>
+		<SafeAreaView style={{ flex: 1 }}>
+			<View style={screenStyles.hero}>
+				<View>
+					<View>
+						<Text>Location</Text>
+						<Text>Los Angeles, US</Text>
+					</View>
+					<View>{/* image here */}</View>
+				</View>
+				<View>
+					{/* icon */}
+					<TextInput />
+					<View>{/* icon */}</View>
+				</View>
+			</View>
 		</SafeAreaView>
 	);
 }
+
+const screenStyles = StyleSheet.create({
+	hero: {
+		backgroundColor: '#131313',
+		height: '30%',
+	},
+});
